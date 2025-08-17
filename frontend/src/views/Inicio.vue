e5ced7b7051ffac45c7d375c9681c6ba34c004b2
KevinPradaSSJ <santicastellanos2017@gmail.com>
2025-08-17 16:36:20 -0500

    Creando componente titulo

--- frontend/src/views/Inicio.vue
+++ frontend/src/views/Inicio.vue
@@ -1,11 +1,13 @@
 <script setup>
 import Encabezado from '../components/encabezado.vue';
+import Titulo from '../components/titulo-club.vue'
 import Pie from '../components/pie.vue';
 </script>
 
 <template>
   <main>
     <Encabezado rol="Entrenador"/>
+    <Titulo />
     <Pie />
   </main>
 </template>
