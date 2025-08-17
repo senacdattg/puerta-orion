da59531c98242ea480760100e4886b53557aa11d
KevinPradaSSJ <santicastellanos2017@gmail.com>
2025-08-17 17:22:38 -0500

    Creando vista de mensualidades y su componente tabla mensualidades

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -7,6 +7,7 @@ import RegistrarGeneral from '../views/registrar-general.vue'
 import VerGeneral from '../views/ver-general.vue'
 import ActualizarGeneral from '../views/actualizar-general.vue'
 import VerRoles from '../views/vista-roles.vue'
+import TablaMensualidades from '../views/mensualidades.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -50,6 +51,11 @@ const router = createRouter({
       path: '/ver-roles',
       name: 'ver-roles',
       component: VerRoles,
+    },
+    {
+      path: '/mensualidades',
+      name: 'mensualidades',
+      component: TablaMensualidades
     }
   ],
 })
---
+++ frontend/src/views/mensualidades.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Encabezado from '../components/encabezado.vue';
+import TablaMensualidades from '../components/tabla-mensualidades.vue';
+import Pie from '../components/pie.vue';
+</script>
+
+<template>
+  <main>
+    <Encabezado rol="Entrenador"/>
+    <TablaMensualidades />
+    <Pie />
+  </main>
+</template>
