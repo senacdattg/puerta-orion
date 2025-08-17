6d3237faae0e4eb0c84a83aa875fa189ff7f2e52
KevinPradaSSJ <santicastellanos2017@gmail.com>
2025-08-16 23:55:51 -0500

    Terminando vistas generales, aun falta acomodar la vista general para el acudiente

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,6 +3,9 @@ import Inicio from '../views/Inicio.vue'
 import ActualizarDeportista from '../views/actualizar-deportista.vue'
 import RegistrarDeportista from '../views/registrar-deportista.vue'
 import VerDeportista from '../views/ver-deportista.vue'
+import RegistrarGeneral from '../views/registrar-general.vue'
+import VerGeneral from '../views/ver-general.vue'
+import ActualizarGeneral from '../views/actualizar-general.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,20 +16,35 @@ const router = createRouter({
       component: Inicio,
     },
     {
-      path: '/actualizar',
-      name: 'actualizar',
+      path: '/actualizar-deportista',
+      name: 'actualizar-deportista',
       component: ActualizarDeportista,
     },
     {
-      path: '/registrar',
-      name: 'registrar',
+      path: '/registrar-deportista',
+      name: 'registrar-deportista',
       component: RegistrarDeportista,
     },
     {
-      path: '/ver',
-      name: 'ver',
+      path: '/ver-deportista',
+      name: 'ver-deportista',
       component: VerDeportista,
     },
+    {
+      path: '/registrar-general',
+      name: 'registrar-general',
+      component: RegistrarGeneral,
+    },
+    {
+      path: '/ver-general',
+      name: 'ver-general',
+      component: VerGeneral,
+    },
+    {
+      path: '/actualizar-general',
+      name: 'actualizar-general',
+      component: ActualizarGeneral,
+    }
   ],
 })
 
