6ab6db55463a6166bece844a6754840e97f7352d
KevinPradaSSJ <santicastellanos2017@gmail.com>
2025-08-17 19:05:01 -0500

    Cambiando algunas vistas

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -11,6 +11,7 @@ import '@fortawesome/fontawesome-free/css/all.min.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
+
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
